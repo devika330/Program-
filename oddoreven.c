193bba4bee2cd98eedbdd849dd74615e14093fd6
Devika B <109600239+devika330@users.noreply.github.com>
2022-09-14 04:21:16 -0700

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int num,count=0;
+    printf("Enter the Number=");
+    scanf("%d",&num);
+    if(num%2==0)
+         count=1;
+    if(count==1)
+    {
+       printf("It is Even Number");
+       printf("\nThe Even Number is %d",num);
+    }
+    else
+    {
+       printf("It is Odd Number");
+       printf("\nThe Odd Number is %d",num);
+    }
+       
+ }

0fb2f0658a3d3f178cc3420552f3b9705f0c08ca
Devika B <109600239+devika330@users.noreply.github.com>
2022-09-14 04:29:01 -0700

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int num1,num2,add;
+    printf("Enter the Number1=");
+    scanf("%d",&num1);
+    printf("Enter the Number2=");
+    scanf("%d",&num2);
+    add=num1+num2;
+    printf("%d + %d is %d",num1,num2,add);
+ }
